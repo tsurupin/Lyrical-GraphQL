409f711052aadd1b7d1278a89e42fa5a0b07d5df
Tomoaki Tsuruta <england0112@gmail.com>
2018-01-15 13:05:25 -0800

    Finish chapter 9

--- client/index.js
+++ client/index.js
@@ -1,8 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import ApolloClient from 'apollo-client';
+import { Router, Route, hashHistory, IndexRoute } from 'react-router';
+import { ApolloProvider } from 'react-apollo';
+import SongList from './components/SongList';
+import SongCreate from './components/SongCreate';
+import App from './components/App';
+
+const client = new ApolloClient({});
 
 const Root = () => {
-  return <div>Lyrical</div>
+
+  return (
+    <ApolloProvider client={client}>
+      <Router history={hashHistory}>
+        <Route path="/" component={App}>
+          <IndexRoute component={SongList} />
+          <Route path="songs/new" component={SongCreate} />
+        </Route>
+      </Router>
+    </ApolloProvider>
+  )
 };
 
 ReactDOM.render(
